866558ad1f2fa2392bf897db7f20c25fd269cdaa
Maurizio Turatti <maurizio@softinstigate.com>
2026-08-10 18:37:12 +0200

    refactor: improve readability and organization of JsPluginsIT class

--- core/src/test/java/org/facet/integration/JsPluginsIT.java
+++ core/src/test/java/org/facet/integration/JsPluginsIT.java
@@ -1,7 +1,11 @@
 package org.facet.integration;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 
 import java.time.Duration;
 
@@ -38,6 +42,7 @@ class JsPluginsIT {
 
     private static final int RESTHEART_PORT = 8080;
 
+    @SuppressWarnings({ "resource", "null" })
     @Container
     private static final GenericContainer<?> MONGO = new GenericContainer<>(DockerImageName.parse("mongo:8.0"))
             .withNetwork(NETWORK)
@@ -46,32 +51,39 @@ class JsPluginsIT {
             .waitingFor(Wait.forLogMessage(".*Waiting for connections.*", 1)
                     .withStartupTimeout(Duration.ofSeconds(30)));
 
+    @SuppressWarnings({ "resource", "null" })
     @Container
-    private static final GenericContainer<?> RESTHEART = new GenericContainer<>(DockerImageName.parse("facet-test:latest"))
-            .withNetwork(NETWORK)
-            .dependsOn(MONGO)
-            .withExposedPorts(RESTHEART_PORT)
-            .withEnv("RHO",
-                    "/mclient/connection-string->\"mongodb://mongodb\";"
-                            + "/http-listener/host->\"0.0.0.0\";")
-            .withCommand("-o", "/opt/restheart/etc/restheart.yml")
-            .withCopyFileToContainer(
-                    MountableFile.forHostPath("../examples/product-catalog/plugins/product-stats"),
-                    "/opt/restheart/plugins/product-stats/")
-            .withCopyFileToContainer(
-                    MountableFile.forHostPath("../examples/product-catalog/plugins/request-logger"),
-                    "/opt/restheart/plugins/request-logger/")
-            .withCopyFileToContainer(
-                    MountableFile.forHostPath("../examples/product-catalog/templates"),
-                    "/opt/restheart/templates/")
-            .withCopyFileToContainer(
-                    MountableFile.forHostPath("../examples/product-catalog/restheart.yml"),
-                    "/opt/restheart/etc/restheart.yml")
-            .withCopyFileToContainer(
-                    MountableFile.forHostPath("../examples/product-catalog/users.yml"),
-                    "/opt/restheart/etc/users.yml")
-            .waitingFor(Wait.forHttp("/ping").forPort(RESTHEART_PORT)
-                    .withStartupTimeout(Duration.ofSeconds(60)));
+    private static final GenericContainer<?> RESTHEART = new GenericContainer<>(
+            DockerImageName.parse("facet-test:latest"))
+                    .withNetwork(NETWORK)
+                    .dependsOn(MONGO)
+                    .withExposedPorts(RESTHEART_PORT)
+                    .withEnv("RHO",
+                            "/mclient/connection-string->\"mongodb://mongodb\";"
+                                    + "/http-listener/host->\"0.0.0.0\";")
+                    .withCommand("-o", "/opt/restheart/etc/restheart.yml")
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath(
+                                    "../examples/product-catalog/plugins/product-stats"),
+                            "/opt/restheart/plugins/product-stats/")
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath(
+                                    "../examples/product-catalog/plugins/request-logger"),
+                            "/opt/restheart/plugins/request-logger/")
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath(
+                                    "../examples/product-catalog/templates"),
+                            "/opt/restheart/templates/")
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath(
+                                    "../examples/product-catalog/restheart.yml"),
+                            "/opt/restheart/etc/restheart.yml")
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath(
+                                    "../examples/product-catalog/users.yml"),
+                            "/opt/restheart/etc/users.yml")
+                    .waitingFor(Wait.forHttp("/ping").forPort(RESTHEART_PORT)
+                            .withStartupTimeout(Duration.ofSeconds(60)));
 
     @BeforeAll
     static void setUp() {
@@ -80,7 +92,7 @@ class JsPluginsIT {
             MONGO.execInContainer("mongosh", "--quiet", "--eval",
                     "try { rs.initiate() } catch(e) {}");
             Thread.sleep(5000); // Wait for replica set election
-        } catch (Exception e) {
+        } catch (Exception _) {
             // Ignore
         }
 
